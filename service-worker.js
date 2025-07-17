bf36db9849ba1fa12e17b25839b8db67de7c3dd5
m97jk <juliusmeier1997@web.de>
2025-07-17 16:21:01 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,18 @@
+
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open("strichliste-cache").then(cache => {
       return cache.addAll([
-        "strichliste.html",
+        "index_mit_speicher_passwort.html",
         "manifest.json",
         "icon-192.png",
-        "icon-512.png"
+        "icon-512.png",
+        "https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"
       ]);
     })
   );
 });
+
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
